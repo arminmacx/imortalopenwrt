a5746906e57245bfdffc74b40db8e75f86ce060b
Armin <visualarmin@gmail.com>
2021-05-14 12:37:07 +0430

    Update before_build.sh

--- before_build.sh
+++ before_build.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 cd /home/runner/immortalwrt
 
+sed -i '$a src-git small https://github.com/kenzok8/small.git' feeds.conf.default
+sed -i '$a src-git kenzo https://github.com/kenzok8/openwrt-packages.git' feeds.conf.default
+sed -i '$a src-git proxypkg https://github.com/kenzok8/small-package' feeds.conf.default
 # Enter your commands here, e.g.
 # echo "Start build!"
 make defconfig
