7476013348277dd32be9f78c9310840b8518cbbd
Armin <visualarmin@gmail.com>
2021-05-14 13:01:33 +0430

    Update clone_source.sh

--- clone_source.sh
+++ clone_source.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 
 git clone "https://github.com/immortalwrt/immortalwrt.git" --branch "openwrt-19.07" --single-branch "immortalwrt"
+sed -i '$a src-git small https://github.com/kenzok8/small.git' feeds.conf.default
+sed -i '$a src-git kenzo https://github.com/kenzok8/openwrt-packages.git' feeds.conf.default
+sed -i '$a src-git proxypkg https://github.com/kenzok8/small-package' feeds.conf.default
 pushd "immortalwrt"; ./scripts/feeds update -a; ./scripts/feeds install -a; popd
